6be388e9e0a428807300221bc7f3167cfc8e6c4c
Paul Rich <paulrichdev@gmail.com>
2021-03-25 20:32:50 -0700

    created allTrue.js - solved

---
+++ allTrue.js
@@ -0,0 +1,6 @@
+function truthCheck(collection, pre) {
+  // Return true if every element in the collection has a truthy value for the provided attribute
+    return collection.every(obj => obj[pre]);
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
